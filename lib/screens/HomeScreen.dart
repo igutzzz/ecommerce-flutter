5d542ca4ce8bbcfb8fa10711decf27f08ba2beae
igutzzz <igor.almeida@madeinweb.app>
2023-07-27 09:57:52 -0300

    feat: function to read the json data

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -1,18 +1,39 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List wines = [];
+
+  Future<void> readJson() async {
+    final String response =
+        await rootBundle.loadString("assets/data/wines.json");
+    final data = await jsonDecode(response);
+    setState(() {
+      wines = data["wines"];
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    readJson();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        appBarTheme: AppBarTheme(
-          backgroundColor: Colors.green[700]
-        ),
-        textTheme: GoogleFonts.poppinsTextTheme()
-      ),
+          appBarTheme: AppBarTheme(backgroundColor: Colors.green[700]),
+          textTheme: GoogleFonts.poppinsTextTheme()),
       home: Scaffold(
         appBar: AppBar(
           leading: BackButton(
